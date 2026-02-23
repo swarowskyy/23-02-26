7d9fb368c93825dfe78aff173c8da14dd08dfb81
Luiza Swarowsky <luiza_swarowsky@estudante.sesisenai.org.br>
2026-02-23 16:05:38 -0300

    c

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
 const texto=document.getElementById("texto")
 const botao=document.getElementById("botao")
-const cupom=document.getElementById("cupom")
 const senha="dash15"
 
 botao.addEventListener("click",()=>{
-    if (cupom==senha){
-
-        texto.textContent= "Você ganhou 10% de desconto "
+    const codigo = document.getElementById("cupom").value.trim()
+    if (codigo==senha){
+        texto.textContent= "Você ganhou 10% de desconto em sua compra 🤩 "
     }else{ 
         texto.textContent="código inválido"
+        botao.textContent="resgatar brinde"
+
     }
-    botao.textContent="resgatado."
+  
 });
