a9315945ae702e76b12e33b4f1e84d877a2b3284
Luiza Swarowsky <luiza_swarowsky@estudante.sesisenai.org.br>
2026-02-23 14:34:06 -0300

    .

--- scripts.js
+++ scripts.js
@@ -1,1 +1,5 @@
-alert("oi ")
+const texto=document.getElementById("texto")
+const botao=document.getElementById("botao")
+botao.addEventListener("click",()=>{
+    texto.textContent= "Você ja esta ganhando conhecimento"
+});
